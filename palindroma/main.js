726d88e9a960e9235a110f3e05c50ad9b9b0d8e0
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-05-07 15:41:18 +0200

    [UPD] Added palindrome script

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,4 +1,30 @@
 'use strict';
 
+//Get word from user
+const userWord = prompt('Inserisci una parola');
 
+if (palindromeCheck(userWord)) {
+    console.log('La parola è palindroma');
+} else {
+    console.log('La parola non è palindroma');
+}
+
+function palindromeCheck(word) {
+    //Bool declaration for function output
+    let palindromeWord = false;
+
+    //Split string in an array of char
+    const splitWord = word.split("");
+    //Reverse char array
+    const reverseChar = splitWord.reverse();
+    //Join char array to form a string
+    const reverseWord = reverseChar.join("");
+
+    //Check if the initial word is the same as the reverse word
+    if (word === reverseWord) {
+        palindromeWord = true;
+    }
+
+    return palindromeWord;
+}
 
